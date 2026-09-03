075d80865bb97ec9d8261adc43facb554f356eac
zyxoned <denoxyz@proton.me>
2026-09-03 02:15:05 -0400

    Fix JavaScript syntax errors in mouse tracking 3D rotation effect

--- js.js
+++ js.js
@@ -1,19 +1,18 @@
-const img =
-document getElementById('heroImg');
+const img = document.getElementById('heroImg');
 let targetRot = 0;
 let currentRot = 0;
 const lag = 0.3275;
 
-document.addEventListener('mousemove',(e) => {
-    const center=window.innerWidth/2;
-    const offset = (e.clientX - center)/center;
-    targetRot = offset*15;
+document.addEventListener('mousemove', (e) => {
+    const center = window.innerWidth / 2;
+    const offset = (e.clientX - center) / center;
+    targetRot = offset * 15;
 });
 
 function animate() {
-    currentRot +=(tagertRot - currentRot) * lag;
-    img.style.transform = rotateY(${currentRot}deg);
-    requestAnimationframe(Animate);
+    currentRot += (targetRot - currentRot) * lag;
+    img.style.transform = `rotateY(${currentRot}deg)`;
+    requestAnimationFrame(animate);
 }
 
 animate();
