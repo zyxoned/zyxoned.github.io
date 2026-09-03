ff8afced5ec51e6f1edf4534b79ccaf8e6518d49
zyxoned <denoxyz@proton.me>
2026-09-03 00:06:48 -0400

    Create js.js
    attempt for your background image following mouse

---
+++ js.js
@@ -0,0 +1,19 @@
+const img =
+document getElementById('heroImg');
+let targetRot = 0;
+let currentRot = 0;
+const lag = 0.3275;
+
+document.addEventListener('mousemove',(e) => {
+    const center=window.innerWidth/2;
+    const offset = (e.clientX - center)/center;
+    targetRot = offset*15;
+});
+
+function animate() {
+    currentRot +=(tagertRot - currentRot) * lag;
+    img.style.transform = rotateY(${currentRot}deg);
+    requestAnimationframe(Animate);
+}
+
+animate();
